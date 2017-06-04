1d93729f2df01d8d922ad05cf14d07922023c2dd
Nicholas Flintham <nick@flinny.org>
2017-06-04 11:14:07 +0100

    Update kernel from Gen_Unlock_1.11.617.1 build - fixes screen/touch on initial boot Actually copy exfat module to the build

--- twrp_ocn.mk
+++ twrp_ocn.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
+# Copy kernel module for exfat
+    recovery/root/system/lib/modules/texfat.ko:recovery/root/system/lib/modules/texfat.ko
+
 $(call inherit-product, device/htc/ocn/aosp_ocn.mk)
 $(call inherit-product-if-exists, device/htc/ocn/omni_ocn.mk)
 
