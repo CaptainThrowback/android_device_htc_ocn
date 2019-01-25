3f092cc5b4a22907cfac463184774ee5041a36b1
Captain Throwback <captainthrowback@hotmail.com>
2019-01-25 15:56:26 -0500

    ocn: remove redundant ro.product.model resetprops
    already set in makefile as the default value
    only change when model is different from default

--- recovery/root/sbin/runatboot.sh
+++ recovery/root/sbin/runatboot.sh
@@ -7,7 +7,6 @@ case $bootmid in
 	"2PZC10000")
 		resetprop ro.build.product "htc_ocnuhl"
 		resetprop ro.product.device "htc_ocnuhl"
-		resetprop ro.product.model "HTC U11"
 		;;
 	"2PZC20000")
 		resetprop ro.build.product "htc_ocnuhljapan"
@@ -22,21 +21,17 @@ case $bootmid in
 	"2PZC40000")
 		resetprop ro.build.product "htc_ocndtwl"
 		resetprop ro.product.device "htc_ocndtwl"
-		resetprop ro.product.model "HTC U11"
 		;;
 	"2PZC50000")
 		resetprop ro.build.product "htc_ocnwhl"
 		resetprop ro.product.device "htc_ocnwhl"
 		if [ $bootcid == 'SPCS_001' ]; then
 			resetprop ro.product.model "2PZC5"
-		else
-			resetprop ro.product.model "HTC U11"
 		fi
 		;;
 	*)
 		resetprop ro.build.product "htc_ocnuhl"
 		resetprop ro.product.device "htc_ocnuhl"
-		resetprop ro.product.model "HTC U11"
 		;;
 esac
 
